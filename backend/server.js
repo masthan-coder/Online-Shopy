701a1933261a0e422954de779174cb58cfa71ef9
khaleel-lab <kbasha968@gmail.com>
2021-12-20 17:53:08 +0530

    routes, error middlewares, importing data to db

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,30 @@
 // ES Module in node
+// NOTE: While importing files make sure to add .js
 
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
-
 import connectDB from './config/db.js';
-import products from './data/products.js';
+
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
-const app = express();
-
 connectDB();
+
+const app = express();
 
 app.get('/', (req, res) => {
 	res.send('API is Running');
 });
 
-app.get('/api/products', (req, res) => {
-	res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-	const product = products.find((p) => p._id === req.params.id);
-	res.json(product);
-});
+// Middlewares for errorHandlers and notFound Route
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
