54a6bee1ab96b2bc136cc8bfe8122282853857fb
khaleel-lab <kbasha968@gmail.com>
2021-12-21 20:43:34 +0530

    user authentication endpoint

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,9 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 
-import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -15,11 +16,15 @@ connectDB();
 
 const app = express();
 
+// allow us to accept the json data in the body
+app.use(express.json());
+
 app.get('/', (req, res) => {
 	res.send('API is Running');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 // Middlewares for errorHandlers and notFound Route
 app.use(notFound);
