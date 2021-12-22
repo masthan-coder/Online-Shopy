186683d42cb4a6fe259ba643c1ddbbf32bff382c
khaleel-lab <kbasha968@gmail.com>
2021-12-22 14:00:39 +0530

    User Registration & Password Encryption

--- backend/models/user.js
+++ backend/models/user.js
@@ -27,7 +27,17 @@ const userSchema = mongoose.Schema(
 	}
 );
 
-// Creating methos to compare the password
+// hasing the password
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) {
+		next();
+	}
+
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Creating method to compare the password
 userSchema.methods.matchPassword = async function (enteredPassword) {
 	return await bcrypt.compare(enteredPassword, this.password);
 };
