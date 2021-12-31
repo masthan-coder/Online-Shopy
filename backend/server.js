25b30ff2d116fa5ac54c0013a82a6424b76dd93b
khaleel-lab <kbasha968@gmail.com>
2021-12-31 08:29:43 +0530

    Image Upload Config & Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 // ES Module in node
 // NOTE: While importing files make sure to add .js
-
+import path from 'path';
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
@@ -10,6 +10,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 
@@ -27,11 +28,16 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 // By using this route - in frontend we can client id
 app.get('/api/config/paypal', (req, res) =>
 	res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+// making a folder static - to load in the browser
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // Middlewares for errorHandlers and notFound Route
 app.use(notFound);
